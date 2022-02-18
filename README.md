f40de80560d8df35987e6ae6b16542aaf1c7818b
Bankole Ojo <ojobankole@gmail.com>
2022-02-18 12:19:17 +0100

    created a shared package and began dividing videos229 to two programs

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Generates videos that could be used as backgrounds of lyrics videos and adverts.
 It expects a background color and a sprite (a repeating image).
 
-It currently has two methods for generating videos.
+It currently has three methods for generating videos.
 
 It's a terminal program.
---
+++ videos229_shared/lib.go
@@ -0,0 +1,45 @@
+package videos229_shared
+
+import (
+  "os"
+  "strings"
+  "github.com/pkg/errors"
+  "path/filepath"
+  "strconv"
+  "image"
+  "image/color"
+  "image/draw"
+)
+
+
+
+func GetRootPath() (string, error) {
+	hd, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.Wrap(err, "os error")
+	}
+	dd := os.Getenv("SNAP_USER_COMMON")
+	if strings.HasPrefix(dd, filepath.Join(hd, "snap", "go")) || dd == "" {
+		dd = filepath.Join(hd, "videos229_data")
+    os.MkdirAll(dd, 0777)
+	}
+
+	return dd, nil
+}
+
+
+
+func timeFormatToSeconds(s string) int {
+  // calculate total duration of the song
+  parts := strings.Split(s, ":")
+  minutesPartConverted, err := strconv.Atoi(parts[0])
+  if err != nil {
+    panic(err)
+  }
+  secondsPartConverted, err := strconv.Atoi(parts[1])
+  if err != nil {
+    panic(err)
+  }
+  totalSecondsOfSong := (60 * minutesPartConverted) + secondsPartConverted
+  return totalSecondsOfSong
+}
